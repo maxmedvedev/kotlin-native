8460691fbf7f5bea91aa248d1f2ee6306a1ba3be
Pavel Punegov <32519625+PavelPunegov@users.noreply.github.com>
2019-10-28 17:48:23 +0300

    Fix filter and simplify

--- build-tools/src/main/kotlin/org/jetbrains/kotlin/ExecutorService.kt
+++ build-tools/src/main/kotlin/org/jetbrains/kotlin/ExecutorService.kt
@@ -353,13 +353,13 @@ private fun deviceLauncher(project: Project) = object : ExecutorService {
                 .also { if (project.verboseTest) println("STDOUT:\n$it") }
                 .split("\n")
                 .run { drop(indexOfFirst { s -> s.startsWith("(lldb) process launch") } + 1) }
+                .run { dropLast(size - indexOf("(lldb) get_exit_code")) }
                 .filterNot {
                     it.startsWith("(lldb)") ||
                             it.matches("Process [0-9]* exited with status .*".toRegex()) ||
                             it.matches("Process [0-9]* launched.*".toRegex())
                 }
-                .flatMap { it.toByteArray().toList() }
-                .toTypedArray()
+                .joinToString("\n")
                 .also {
                     savedOut?.write(it.toByteArray())
                 }
